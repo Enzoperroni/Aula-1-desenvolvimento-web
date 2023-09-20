9d6f21f30daa07febbbff74dda77555086891623
Enzo Perroni <142441388+Enzoperroni@users.noreply.github.com>
2023-09-20 14:59:35 -0300

    Add files via upload

---
+++ file.js
@@ -0,0 +1,23 @@
+let primeira = 1;
+let segunda ="casa";
+const terceira = [1,2,"nada"];
+
+let quarta = {
+  primeiro: 1,
+    segundo: 'tudo'
+};
+
+console.log(primeira +"primeira: ")
+terceira.push("novo")
+
+segunda = "banana"
+console.log("segunda: " + segunda)
+
+console.log("terceira: " + terceira)
+console.log("quarta.primeiro: " +quarta.primeiro)
+
+const outrafuncao = (entrada) => {
+  return entrada+3
+}
+
+console.log(outrafuncao(1))
